4360c087bad1a1dd9be1e0c752c97e02f31d9f83
beomjung <k980220@naver.com>
2022-07-13 14:22:39 +0900

    [#1] feature : 회원관리 CRUD

---
+++ src/main/java/kopo/poly/controller/UserController.java
@@ -0,0 +1,9 @@
+package kopo.poly.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@Slf4j
+public class UserController {
+}
---
+++ src/main/java/kopo/poly/service/impl/UserService.java
@@ -0,0 +1,10 @@
+package kopo.poly.service.impl;
+
+import kopo.poly.service.IUserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class UserService implements IUserService {
+}
