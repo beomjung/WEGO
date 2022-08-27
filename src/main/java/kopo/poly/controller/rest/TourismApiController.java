eb4cadbc5ee7cc1012becaa0b329c957b0a1d30f
JeongSeonggil <sgjeong1108@gmail.com>
2022-08-27 20:34:56 +0900

    [#18] feature : API Controller EndPoint

--- src/main/java/kopo/poly/controller/rest/TourismApiController.java
+++ src/main/java/kopo/poly/controller/rest/TourismApiController.java
@@ -1,8 +1,7 @@
 package kopo.poly.controller.rest;
 
 import kopo.poly.dto.api.ApiLodgingDto;
-import kopo.poly.dto.api.introductions.TransportationResult;
-import kopo.poly.dto.api.introductions.TravelCourseResult;
+import kopo.poly.dto.api.introductions.*;
 import kopo.poly.enums.LanguageType;
 import kopo.poly.factory.ApiServiceFactory;
 import kopo.poly.service.ITourismApiService;
@@ -26,6 +25,15 @@ import static kopo.poly.constant.ApiConstants.LANGUAGE_TYPE_HEADER;
 public class TourismApiController {
     private final ApiServiceFactory serviceFactory;
 
+    /**
+     * 숙박 정보 조회 API 호출
+     * @param languageType
+     * @param areaCode
+     * @param sigunguCode
+     * @param pageNo
+     * @return
+     * @throws Exception
+     */
     @GetMapping("/lodgings/{areaCode}/{sigunguCode}/{pageNo}")
     public ResponseEntity<List<ApiLodgingDto>> getLodgingInfo(
             @RequestHeader(LANGUAGE_TYPE_HEADER) final LanguageType languageType,
@@ -39,15 +47,123 @@ public class TourismApiController {
 
     }
 
+    //
+    // ######################### 소개 정보 조회 API 호출 #########################
+    //
+
+    /**
+     * 관광지 정보 조회
+     *
+     * @param languageType
+     * @param contentId
+     * @return
+     * @throws Exception
+     */
+    @GetMapping("/introductions/tourist-destination/{contentId}")
+    public ResponseEntity<List<TouristDestinationResult>> getTouristDestination(
+            @RequestHeader(LANGUAGE_TYPE_HEADER) final LanguageType languageType, @PathVariable final String contentId)
+            throws Exception {
+        final ITourismApiService tourismApiService = serviceFactory.findTourismApiService(languageType);
+
+        return ResponseEntity.ok().body(tourismApiService.getTouristDestinationList(languageType, contentId));
+    }
+
+    /**
+     * 문화 시설 조회
+     *
+     * @param languageType
+     * @param contentId
+     * @return
+     * @throws Exception
+     */
+    @GetMapping("/introductions/cultural-facilities/{contentId}")
+    public ResponseEntity<List<CulturalFacilitiesResult>> getCulturalFacilities(
+            @RequestHeader(LANGUAGE_TYPE_HEADER) final LanguageType languageType, @PathVariable final String contentId)
+            throws Exception {
+        final ITourismApiService tourismApiService = serviceFactory.findTourismApiService(languageType);
+
+        return ResponseEntity.ok().body(tourismApiService.getCulturalFacilitiesList(languageType, contentId));
+    }
+
+
+    /**
+     * 축제 정보 조회
+     *
+     * @param languageType
+     * @param contentId
+     * @return
+     * @throws Exception
+     */
+    @GetMapping("/introductions/festival/{contentId}")
+    public ResponseEntity<List<FestivalInfoResult>> getFestivalInfo(
+            @RequestHeader(LANGUAGE_TYPE_HEADER) final LanguageType languageType, @PathVariable final String contentId)
+            throws Exception {
+        final ITourismApiService tourismApiService = serviceFactory.findTourismApiService(languageType);
+
+        return ResponseEntity.ok().body(tourismApiService.getFestivalInfoList(languageType, contentId));
+    }
+
+    /**
+     * 레포츠 정보 조회
+     * @param languageType
+     * @param contentId
+     * @return
+     * @throws Exception
+     */
+    @GetMapping("/introductions/leports/{contentId}")
+    public ResponseEntity<List<LeportsResult>> getLeports(
+            @RequestHeader(LANGUAGE_TYPE_HEADER) final LanguageType languageType, @PathVariable final String contentId)
+            throws Exception {
+        final ITourismApiService tourismApiService = serviceFactory.findTourismApiService(languageType);
+
+        return ResponseEntity.ok().body(tourismApiService.getLeportsList(languageType, contentId));
+    }
+
+    /**
+     * 쇼핑 정보 조회
+     * @param languageType
+     * @param contentId
+     * @return
+     * @throws Exception
+     */
+    @GetMapping("/introductions/shopping/{contentId}")
+    public ResponseEntity<List<ShoppingResult>> getShopping(
+            @RequestHeader(LANGUAGE_TYPE_HEADER) final LanguageType languageType, @PathVariable final String contentId)
+            throws Exception {
+        final ITourismApiService tourismApiService = serviceFactory.findTourismApiService(languageType);
+
+        return ResponseEntity.ok().body(tourismApiService.getShoppingList(languageType, contentId));
+    }
+
+
+    /**
+     * 음식점 정보 조회
+     * @param languageType
+     * @param contentId
+     * @return
+     * @throws Exception
+     */
+    @GetMapping("/introductions/restaurants/{contentId}")
+    public ResponseEntity<List<RestaurantsResult>> getRestaurants(
+            @RequestHeader(LANGUAGE_TYPE_HEADER) final LanguageType languageType, @PathVariable final String contentId)
+            throws Exception {
+        final ITourismApiService tourismApiService = serviceFactory.findTourismApiService(languageType);
+
+        return ResponseEntity.ok().body(tourismApiService.getRestaurantsList(languageType, contentId));
+    }
+
+
+
     /**
      * 교통 정보 조회
+     *
      * @param languageType
      * @param contentId
      * @return
      * @throws Exception (국문 서비스는 제공하지 않기 때문에 KOR 로 요청 시 Exception)
      */
     @GetMapping("/introductions/transportation/{contentId}")
-    public ResponseEntity<List<TransportationResult>> getTransportation (
+    public ResponseEntity<List<TransportationResult>> getTransportation(
             @RequestHeader(LANGUAGE_TYPE_HEADER) final LanguageType languageType, @PathVariable final String contentId)
             throws Exception {
         final ITourismApiService tourismApiService = serviceFactory.findTourismApiService(languageType);
@@ -57,13 +173,14 @@ public class TourismApiController {
 
     /**
      * 여행 코스 정보 조회
+     *
      * @param languageType
      * @param contentId
      * @return
      * @throws Exception (다국어 서비스로 요청 시 Exception)
      */
     @GetMapping("/introductions/travel-course/{contentId}")
-    public ResponseEntity<List<TravelCourseResult>> getTravelCourse (
+    public ResponseEntity<List<TravelCourseResult>> getTravelCourse(
             @RequestHeader(LANGUAGE_TYPE_HEADER) final LanguageType languageType, @PathVariable final String contentId)
             throws Exception {
         final ITourismApiService tourismApiService = serviceFactory.findTourismApiService(languageType);
